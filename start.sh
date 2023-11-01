384a882a9b917a1d9d711022376e7f879d35edca
bovivian <121907308+bovivian@users.noreply.github.com>
2023-11-01 20:29:08 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker run -it --user ros --gpus all --network=host --ipc=host --name pgr_dv -v $PWD/shared_folder:/shared_folder -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY ros2_pgr_dv
+sudo docker run --rm -it --user ros --gpus all --network=host --ipc=host --name pgr_dv -v $PWD/shared_folder:/shared_folder -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY ros2_pgr_dv
 
