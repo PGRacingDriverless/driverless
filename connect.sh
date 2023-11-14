02141dc2f7c5dc9f6c3858ca89fbf955241e3512
ntafel <119122143+ntafel@users.noreply.github.com>
2023-11-14 14:01:00 +0100

    docker group checking for sudo

--- connect.sh
+++ connect.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-sudo docker exec -it pgr_dv bash
+docker_group_name=docker
+
+docker_exec() {
+	docker exec -it pgr_dv bash
+}
+
+if groups "$USER" | grep -qw "$docker_group_name"; then
+	docker_run
+else
+	sudo bash -c "$(declare -f docker_exec); docker_exec"
+fi
--- start.sh
+++ start.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-sudo docker run --rm -it --user ros --gpus all --network=host --ipc=host --name pgr_dv -v $PWD/shared_folder:/shared_folder -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY ros2_pgr_dv
+docker_group_name=docker
+
+docker_run() {
+    docker run -it \
+			--rm \
+			-v $PWD/ws:/home/ros/ws \
+			-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+			--env=DISPLAY \
+			--gpus=all \
+			--ipc=host \
+			--name=pgr_dv \
+			--network=host \
+			--user=ros \
+		ros2_pgr_dv
+}
+
+if groups "$USER" | grep -qw "$docker_group_name"; then
+	docker_run
+else
+	sudo bash -c "$(declare -f docker_run); docker_run"
+fi
 
