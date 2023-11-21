7d439c031a06d0389e096eb215a6660903f55b77
ntafel <119122143+ntafel@users.noreply.github.com>
2023-11-21 22:00:13 +0100

    WSL support

--- run.sh
+++ run.sh
@@ -15,12 +15,33 @@ readonly CLR_END="\033[0m"
 
 readonly MSG_WITH_SUDO="[INFO] $(timestamp) User $USER is not added to the $DOCKER_GROUP_NAME group. Running with sudo."
 readonly MSG_WITHOUT_SUDO="[INFO] $(timestamp) User $USER has been added to the $DOCKER_GROUP_NAME group. Running without sudo."
-readonly MSG_NOT_RUNNING="[INFO] $(timestamp) The container named $DOCKER_CONTAINER_NAME is not running. Launching it."
 readonly MSG_RUNNING="[INFO] $(timestamp) A container named $DOCKER_CONTAINER_NAME is already running. Running bash shell for it."
+readonly MSG_NOT_RUNNING="[INFO] $(timestamp) The container named $DOCKER_CONTAINER_NAME is not running. Launching it."
 
 container_run() {
-	echo $DOCKER_IMAGE_NAME
-    docker run -it \
+	readonly MSG_NATIVE_LINUX="[INFO] `date +"%H:%M:%S"` WSL was not detected. Running as on native Linux."
+	readonly MSG_WSL_LINUX="[INFO] `date +"%H:%M:%S"` WSL distribution of Linux is detected. Run with parameters for Windows."
+
+	if grep -qi microsoft /proc/version; then
+		echo $MSG_WSL_LINUX
+		docker run -it \
+			--rm \
+			-v $PWD/../ws:/home/ros/ws \
+			-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+			-v /mnt/wslg:/mnt/wslg \
+			-e DISPLAY=:0 \
+			-e WAYLAND_DISPLAY=wayland-0 \
+			-e XDG_RUNTIME_DIR=/mnt/wslg/runtime-dir \
+			-e PULSE_SERVER=/mnt/wslg/PulseServer \
+			--gpus=all \
+			--ipc=host \
+			--name=$1 \
+			--network=host \
+			--user=ros \
+		$2
+	else
+		echo $MSG_NATIVE_LINUX
+		docker run -it \
 			--rm \
 			-v $PWD/../ws:/home/ros/ws \
 			-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
@@ -31,6 +52,7 @@ container_run() {
 			--network=host \
 			--user=ros \
 		$2
+	fi
 }
 
 container_exec_bash() {
