82ff7bc6ec77efa9fd9d703de0a8a98352fc34ef
ntafel <119122143+ntafel@users.noreply.github.com>
2023-11-21 20:17:20 +0100

    run script improves

--- run.sh
+++ run.sh
@@ -4,6 +4,17 @@ readonly DOCKER_GROUP_NAME=docker
 readonly DOCKER_IMAGE_NAME=ros2_pgr_dv
 readonly DOCKER_CONTAINER_NAME=pgr_dv
 
+readonly CLR_RED="\033[0;31m"
+readonly CLR_GREEN="\033[0;32m"
+readonly CLR_YELLOW="\033[0;33m"
+readonly CLR_END="\033[0m"
+
+readonly MSG_WITH_SUDO="[INFO] $(timestamp) User $USER is not added to the $DOCKER_GROUP_NAME group. Running with sudo."
+readonly MSG_WITHOUT_SUDO="[INFO] $(timestamp) User $USER has been added to the $DOCKER_GROUP_NAME group. Running without sudo."
+readonly MSG_NOT_RUNNING="[INFO] $(timestamp) The container named $DOCKER_CONTAINER_NAME is not running. Launching it."
+readonly MSG_RUNNING="[INFO] $(timestamp) A container named $DOCKER_CONTAINER_NAME is already running. Running bash shell for it."
+
+
 timestamp() {
 	date +"%H:%M:%S"
 }
@@ -26,41 +37,24 @@ container_exec_bash() {
 	docker exec -it pgr_dv bash
 }
 
-if groups "$USER" | grep -qw "$DOCKER_GROUP_NAME";
-then
-	echo "[INFO]: $(timestamp)"\
-	"User $USER has been added to the $DOCKER_GROUP_NAME group."\
-	"Running without sudo."
+if groups "$USER" | grep -qw "$DOCKER_GROUP_NAME"; then
+	echo $MSG_WITHOUT_SUDO
 	
-	if [ "$(docker ps -aq -f status=running -f name=$DOCKER_CONTAINER_NAME)" ];
-	then
-		echo "[INFO]: $(timestamp)"\
-		"A container named $DOCKER_CONTAINER_NAME is already running."\
-		"Running bash shell for it."
-
+	if [ "$(docker ps -aq -f status=running -f name=$DOCKER_CONTAINER_NAME)" ]; then
+		echo $MSG_RUNNING
 		container_exec_bash
 	else
-		echo "[INFO]: $(timestamp)"\
-		"The container named $DOCKER_CONTAINER_NAME is not running. Launching it."
-
+		echo $MSG_NOT_RUNNING
 		container_run
 	fi
 else
-	echo "[INFO]: $(timestamp)"\
-	"User $USER is not added to the $DOCKER_GROUP_NAME group."\
-	"Running with sudo."
-	
-	if [ "$(sudo docker ps -aq -f status=running -f name=$DOCKER_CONTAINER_NAME)" ];
-	then
-		echo "[INFO]: $(timestamp)"\
-		"A container named $DOCKER_CONTAINER_NAME is already running."\
-		"Running bash shell for it."
+	echo $MSG_WITH_SUDO
 
+	if [ "$(sudo docker ps -aq -f status=running -f name=$DOCKER_CONTAINER_NAME)" ]; then
+		echo $MSG_RUNNING
 		sudo bash -c "$(declare -f container_exec_bash); container_exec_bash"
 	else
-		echo "[INFO]: $(timestamp)"\
-		"The container named $DOCKER_CONTAINER_NAME is not running. Launching it."
-
+		echo $MSG_NOT_RUNNING
 		sudo bash -c "$(declare -f container_run); container_run"
 	fi
 fi
